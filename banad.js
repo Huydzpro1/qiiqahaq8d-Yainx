775f66d6e8b34257de4b22d26b4c552c6ac85c91
Huydzpro1 <97621092+Huydzpro1@users.noreply.github.com>
2022-07-14 01:19:49 +0700

    Add files via upload

---
+++ banad.js
@@ -0,0 +1,25 @@
+ //<![CDATA[
+
+(function ignielAdBlock() {
+
+var judulAd = 'Adblock Detected',
+notifAd = 'Please turn off your adblock to continue, after you turn it off then reload this page. <br /><br /><br /> Vui lòng tắt chặn quảng cáo!';
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('5 2=4[\'b\'](\'c\');2[\'p\']=\'q/r\';2[\'s\']=\'//t.u.v/w/e/x.e\';2[\'y\']=f;2[\'z\']=A(){5 a=4[\'b\'](\'6\');a[\'B\']=\'C\';a[\'D\']=\'<6 g="E"><h g="F">\'+G+\'</h><i/><j H="0 0 k k"><l d="I,J,17.K.3,1.3 0 0,1 10.7,m.3,1.3 0 0,1 12,14.L.3,1.3 0 0,1 13.3,m.3,1.3 0 0,1 12,17.M.n,N.O,8.P.Q.R,S.T,15.U.V.n,W"></l></j><i/>\'+X+\'</6>\';4[\'o\'][\'Y\'](a);4[\'o\'][\'Z\'][\'11\']=\'16\';18[\'19\']=f};5 9=4[\'1a\'](\'c\')[0];9[\'1b\'][\'1c\'](2,9)',62,75,'||ad||document|var|div|||b_ad||createElement|script||js|true|class|span|br|svg|24|path|16A1|73|body|type|text|javascript|src|pagead2|googlesyndication|com|pagead|adsbygoogle|async|onerror|function|id|ignielAdBlock|innerHTML|isiAds|judul|judulAd|viewBox|M13|13H11V7H13M12|3A1|7A1|3M15|3H8|27L3|27V15|73L8|27|21H15|73L21|73V8|27L15|3Z|notifAd|append|style||overflow|||||hidden||window|adblock|getElementsByTagName|parentNode|insertBefore'.split('|'),0,{}))
+})();
+
+//]]>
+<style>
+#ignielAdBlock {background:#d74738;padding:0;position:fixed;bottom:0;left:0;top:-100px;right:0;z-index:1000;opacity:1;visibility:visible;height:auto;}
+#ignielAdBlock svg {width:70px; height:70px;padding-bottom:15px;}
+#ignielAdBlock svg path {fill:#fff}
+#ignielAdBlock a{color:#ffe88b;} #ignielAdBlock a:hover{color:#ffe88b;text-decoration:underline}
+#ignielAdBlock .isiAds{background:#e74c3c;color:#fff;position:absolute;text-align:center;padding:0 30px 30px 30px;margin:auto;top:30%;left:0;right:0;font-size:15px;font-weight:400;line-height:1.5em;font-family:monospace;max-width:800px;box-shadow:0 20px 10px -10px #d74738;border:15px solid rgba(0,0,0,.07); overflow:hidden; transition:all .6s cubic-bezier(.25,.8,.25,1); -webkit-transform:translateZ(0); transform:translateZ(0); backface-visibility:visible; transition:all .2s ease-in-out,visibility 0s; transform-origin:bottom center; pointer-events:auto; transform:rotateX(0deg) rotateY(0deg) rotateZ(0deg) scale(1); opacity:1; animation:ignielWobble .5s; -moz-animation: ignielWobble .5s; -webkit-animation:ignielWobble .5s; -o-animation:ignielWobble .5s}
+#ignielAdBlock .isiAds:hover{box-shadow:0 20px 10px -10px #d74738;}
+#ignielAdBlock .isiAds h4, #ignielAdBlock .isiAds .judul{display:inline-block;background:rgba(0,0,0,.07);padding:5px 25px 15px 25px;font-size:23px;font-weight:bold;margin-bottom:20px;}
+#ignielAdBlock .isiAds{margin-top:20px !important;}
+#ignielAdBlock .tutupAds{visibility:hidden;opacity:0;color:#ffe88b;text-align:center;position:absolute;bottom:10px;right:10px;font-size:42px;cursor:pointer;width:30px;height:30px;line-height:30px;border-radius:100%;transition:all .6s cubic-bezier(.25,.8,.25,1);transform:translate(0,50px);}
+#ignielAdBlock .tutupAds:hover{color:#fff;transform:scale(1.0);}
+#ignielAdBlock .isiAds:hover .tutupAds{visibility:hidden;opacity:0;color:#ffe88b;transform:translate(0,0)}
+@keyframes ignielWobble{0%{transform:scale(0) rotate(-54deg) translatey(40px)}45%{transform:scale(1.1) rotate(20deg) translatey(0px)}60%{transform:scale(1) rotate(-15deg) translatey(0px)}77%{transform:scale(1) rotate(10deg) translatey(0px)}100%{transform:translatey(0)}}
+@-webkit-keyframes ignielWobble{0%{transform:scale(0) rotate(-54deg) translatey(40px)}45%{transform:scale(1.1) rotate(20deg) translatey(0px)}60%{transform:scale(1) rotate(-15deg) translatey(0px)}77%{transform:scale(1) rotate(10deg) translatey(0px)}100%{transform:translatey(0)}}
+@-moz-keyframes ignielWobble{0%{transform:scale(0) rotate(-54deg) translatey(40px)}45%{transform:scale(1.1) rotate(20deg) translatey(0px)}60%{transform:scale(1) rotate(-15deg) translatey(0px)}77%{transform:scale(1) rotate(10deg) translatey(0px)}100%{transform:translatey(0)}}</style>
